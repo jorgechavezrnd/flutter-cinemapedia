3e23b484a957bb8cb3c9916d1559a8535a810875
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-06-16 21:40:59 -0400

    Add implementation for providers and configuration for riverpod

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cinemapedia/config/constants/environment.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -8,6 +7,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text(Environment.theMovieDbKey)));
+    return const Scaffold(body: Placeholder());
   }
 }
