0986e268158bce72fe59a7cd27b7c7c3e56cbb73
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-06-22 13:45:58 -0400

    Add widget and configuration for Movie Screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,11 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:cinemapedia/config/theme/app_theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
+  GoRouter.optionURLReflectsImperativeAPIs = true;
 
   runApp(const ProviderScope(child: MainApp()));
 }
