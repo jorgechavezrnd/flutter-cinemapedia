52a38213c5fef4bc794aa3bbf36340728a17a6fe
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-06-23 12:08:40 -0400

    Add home_view and favorites_view

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
 
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:cinemapedia/presentation/screens/screens.dart';
 
 final appRouter = GoRouter(
@@ -8,7 +9,7 @@ final appRouter = GoRouter(
     GoRoute(
       path: '/',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) => const HomeScreen(childView: HomeView()),
       routes: [
         GoRoute(
           path: 'movie/:id',
